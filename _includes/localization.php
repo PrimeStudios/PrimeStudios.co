d12f6b5d6efa10c4becab44082df62f22e6cac8f
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-01-17 17:00:57 -0500

    Delete localization.php

--- _includes/localization.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang = 'en';
-if (file_exists('../language/'.$lang.'.json')) {
-    $langJSON = json_decode(file_get_contents('../language/'.$lang.'.json'));
-} else {
-    $langJSON = json_decode(file_get_contents('../language/en.json'));
-}
