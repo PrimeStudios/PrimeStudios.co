547ae140d4383ecf591af941ab311c7a4defcdec
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-02-08 17:57:45 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 				</ul>
 			</div>
 			<span class="social-media-container">
-				<a href="https://github.com/" target="_blank"><span class="social-media-icon fab fa-github"></span></a>
+				<a href="https://github.com/PrimeStudios" target="_blank"><span class="social-media-icon fab fa-github"></span></a>
 				<a href="https://www.linkedin.com/" target="_blank"><span class="social-media-icon fab fa-linkedin"></span></a>
 			</span>
 			<p class="copyright-text">&copy; 2019-<script>document.write(new Date().getFullYear())</script> Prime Studios. All Rights Reserved.</p>
