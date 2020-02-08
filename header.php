b618a4eb2da6a5804dcff23d76a51d0dd64dae38
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-02-08 07:02:49 -0500

    Header Dropdown

--- header.php
+++ header.php
@@ -2,10 +2,9 @@
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<title>	<?php echo $pageName; ?> - Prime Studios</title>
+		<title><?php echo $pageName; ?> - Prime Studios</title>
 		<meta name="title" content="<?php echo $pageName; ?> | Prime Studios">
-		<meta name="description"
-		 content="Prime Studios is a freelance company focused on building powerful websites for our clients. We strive to provide user-friendly and affordable websites through our service. Whatever your needs, we can fill all your needs with our design team's creativity.">
+		<meta name="description" content="Prime Studios is a freelance company focused on building powerful websites for our clients. We strive to provide user-friendly and affordable websites through our service. Whatever your needs, we can fill all your needs with our design team's creativity.">
 		<meta name="keywords" content="Prime Studios,Web Development,Web Design,Freelancer,Web Freelancer,Web Developer">
 		<meta name="robots" content="index, follow">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -24,6 +23,10 @@
 			<ul>
 				<li <?php if ($pageName=='Home' ) {echo 'class="active"';}?>><a href="/">Home</a></li>
 				<li <?php if ($pageName=='About' ) {echo 'class="active"';}?>><a href="/about">About</a></li>
-				<li>Services <i class="fas fa-angle-down"></i></li>
+				<li class="header-dropdown-button">Services <i class="fas fa-angle-down"></i></li>
+				<div class="header-dropdown">
+					<p class="hd-web-design"><a href="#">Web Design</a></p>
+					<p class="hd-sys-admin"><a href="#">Sys Admin</a></p>
+				</div>
 			</ul>
 		</div>
