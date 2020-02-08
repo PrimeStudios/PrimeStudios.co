b8b5901d4a6dc944e878ede5a247fffd7882991f
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-02-08 04:12:09 -0500

    Footer

--- footer.php
+++ footer.php
@@ -18,8 +18,8 @@
 				</ul>
 			</div>
 			<span class="social-media-container">
-				<a href="#"><span class="social-media-icon fab fa-github"></span></a>
-				<a href="#"><span class="social-media-icon fab fa-linkedin"></span></a>
+				<a href="https://github.com/" target="_blank"><span class="social-media-icon fab fa-github"></span></a>
+				<a href="https://www.linkedin.com/" target="_blank"><span class="social-media-icon fab fa-linkedin"></span></a>
 			</span>
 			<p class="copyright-text">&copy; 2019-<script>document.write(new Date().getFullYear())</script> Prime Studios. All Rights Reserved.</p>
 		</div>
