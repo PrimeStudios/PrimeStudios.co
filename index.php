180a33f6ed6f53852c522cf91a30e5944d034a94
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-02-09 01:13:12 -0500

    Update home page styling

--- index.php
+++ index.php
@@ -9,27 +9,6 @@ include('header.php');
 	<p>Quality, affordable development and design services.</p>
 	<a onclick="document.getElementById('showcase').scrollIntoView({behavior: 'smooth'})"><i class="fas fa-chevron-down"></i></a>
 </div>
-<div id="showcase" class="showcase">
-	<h2>Showcase</h2>
-	<div class="container">
-		<img onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'block'; document.getElementById('showcase_popup_image_two').style.display = 'none'; document.getElementById('showcase_popup_image_three').style.display = 'none'; document.getElementById('showcase_popup_background').style.display = 'block';" class="showcase-item" src="assets/img/showcase-1.png">
-		<img onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'none'; document.getElementById('showcase_popup_image_two').style.display = 'block'; document.getElementById('showcase_popup_image_three').style.display = 'none'; document.getElementById('showcase_popup_background').style.display = 'block';" class="showcase-item" src="assets/img/showcase-2.png">
-		<img onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'none'; document.getElementById('showcase_popup_image_two').style.display = 'none'; document.getElementById('showcase_popup_image_three').style.display = 'block'; document.getElementById('showcase_popup_background').style.display = 'block';" class="showcase-item" src="assets/img/showcase-3.png">
-	</div>
-	<p class="home-project-list"><a class="home-project-link" href="#">View More Here <span class="hover-left-padding fas fa-arrow-circle-right"></span></a></p>
-</div>
-<span onclick="document.getElementById('showcase_popup').style.display ='none'; document.getElementById('showcase_popup_background').style.display = 'none';" id="showcase_popup_background" class="showcase-popup-background"></span>
-<div id="showcase_popup" class="showcase-popup animated zoomIn">
-	<p onclick="document.getElementById('showcase_popup').style.display ='none'; document.getElementById('showcase_popup_background').style.display = 'none';" class="showcase-popup-close">x</p>
-	<img id="showcase_popup_image_one" class="showcase-popup-image" src="assets/img/showcase-1.png">
-	<img id="showcase_popup_image_two" class="showcase-popup-image" src="assets/img/showcase-2.png">
-	<img id="showcase_popup_image_three" class="showcase-popup-image" src="assets/img/showcase-3.png">
-</div>
-<div class="quote">
-	<a href="#">
-		<button>Request a Quote</button>
-	</a>
-</div>
 <div class="features">
 	<div class="card">
 		<i class="fas fa-shipping-fast"></i>
@@ -47,4 +26,32 @@ include('header.php');
 		<p>Our team's diverse set of skills allows us to work effectively and collaboratively to ensure a quality end product.</p>
 	</div>
 </div>
+<div id="showcase" class="showcase">
+	<h2>Showcase</h2>
+	<div class="container">
+		<img
+		 onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'block'; document.getElementById('showcase_popup_image_two').style.display = 'none'; document.getElementById('showcase_popup_image_three').style.display = 'none'; document.getElementById('showcase_popup_background').style.display = 'block';"
+		 class="showcase-item" src="assets/img/showcase-1.png">
+		<img
+		 onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'none'; document.getElementById('showcase_popup_image_two').style.display = 'block'; document.getElementById('showcase_popup_image_three').style.display = 'none'; document.getElementById('showcase_popup_background').style.display = 'block';"
+		 class="showcase-item" src="assets/img/showcase-2.png">
+		<img
+		 onclick="document.getElementById('showcase_popup').style.display = 'block'; document.getElementById('showcase_popup_image_one').style.display = 'none'; document.getElementById('showcase_popup_image_two').style.display = 'none'; document.getElementById('showcase_popup_image_three').style.display = 'block'; document.getElementById('showcase_popup_background').style.display = 'block';"
+		 class="showcase-item" src="assets/img/showcase-3.png">
+	</div>
+	<p class="home-project-list"><a class="home-project-link" href="#">view more</a></p>
+</div>
+<span onclick="document.getElementById('showcase_popup').style.display ='none'; document.getElementById('showcase_popup_background').style.display = 'none';" id="showcase_popup_background" class="showcase-popup-background"></span>
+<div id="showcase_popup" class="showcase-popup animated zoomIn">
+	<p onclick="document.getElementById('showcase_popup').style.display ='none'; document.getElementById('showcase_popup_background').style.display = 'none';" class="showcase-popup-close">x</p>
+	<img id="showcase_popup_image_one" class="showcase-popup-image" src="assets/img/showcase-1.png">
+	<img id="showcase_popup_image_two" class="showcase-popup-image" src="assets/img/showcase-2.png">
+	<img id="showcase_popup_image_three" class="showcase-popup-image" src="assets/img/showcase-3.png">
+</div>
+<div class="quote">
+	<a href="#">
+		<button>Request a Quote</button>
+	</a>
+</div>
+
 <?php include('footer.php'); ?>
