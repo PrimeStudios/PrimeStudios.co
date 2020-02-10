2ea0f9c9691c2d513e665ef202c2dd10073b768d
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-02-10 00:22:03 -0500

    Adds system administration page

---
+++ sysadmin/index.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = 'System Administration Services';
+$pageLevel = 1;
+include('../includes/recursivity.php');
+include('../header.php');
+?>
+<link rel="stylesheet" href="styles.css" />
+<div class="smallhero sysadmin-hero">
+	<h1>Prime Studios</h1>
+	<h2>System Administration Services</h2>
+</div>
+<div class="sysadmin-services">
+	<p>We provide configuration and management for web servers on both Windows and Linux. We're capable of assisting in data and content migration upon request. Configuration also covers the cost of domain registration if necessary. No matter your needs, don't worry about the technical side of your website - rely on us to do the hard part for you. </p>
+	<div class="technologies">
+		<img src="../assets/img/technologies/centos.png" />
+		<img src="../assets/img/technologies/debian.png" />
+		<img src="../assets/img/technologies/fedora.png" />
+		<img src="../assets/img/technologies/linux.png" />
+		<img src="../assets/img/technologies/ubuntu.png" />
+		<img src="../assets/img/technologies/windows.png" />
+	</div>
+</div>
+<div class="quote sysadmin-quote">
+	<a href="/contact">
+		<button>Request a Quote</button>
+	</a>
+</div>
+<?php include('../footer.php'); ?>
