a928758d5a3d181050ef57ddd6c13f7f59679036
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-02-11 19:53:17 -0500

    404

--- 404/index.php
+++ 404/index.php
@@ -7,8 +7,8 @@ include('../header.php');
 <div class="notfound">
 	<p class="notfound-heading">Page Not Found</p>
 	<p class="notfound-description">We couldn't find the requested resource. Try heading <a href="/">back to home</a>.</p>
-		<img src="../assets/img/404-not-found.svg"/>
+	<img src="../assets/img/404-not-found.svg"/>
 </div>
 <div class="not-found-footer">
-<?php include('../footer.php'); ?>
+	<?php include('../footer.php'); ?>
 </div>
