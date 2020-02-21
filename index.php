398402b380a940cb5753d8882386a545576c2dbc
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-02-21 06:14:14 -0500

    Index

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ include('header.php');
 </div>
 <span onclick="document.getElementById('showcase_popup').classList.add('zoomOut'); document.getElementById('showcase_popup_background').style.display = 'none'; setTimeout(function() {document.getElementById('showcase_popup').style.display ='none';}, 300);" id="showcase_popup_background" class="showcase-popup-background"></span>
 <div id="showcase_popup" class="showcase-popup animated zoomIn">
-	<p onclick="document.getElementById('showcase_popup').style.display ='none'; document.getElementById('showcase_popup_background').style.display = 'none'; setTimeout(function() {document.getElementById('showcase_popup').style.display ='none';}, 300);" class="showcase-popup-close">x</p>
+	<p onclick="document.getElementById('showcase_popup').classList.add('zoomOut'); document.getElementById('showcase_popup_background').style.display = 'none'; setTimeout(function() {document.getElementById('showcase_popup').style.display ='none';}, 300);" class="showcase-popup-close">x</p>
 	<img id="showcase_popup_image_one" class="showcase-popup-image" src="assets/img/showcase-1.png">
 	<img id="showcase_popup_image_two" class="showcase-popup-image" src="assets/img/showcase-2.png">
 	<img id="showcase_popup_image_three" class="showcase-popup-image" src="assets/img/showcase-3.png">
