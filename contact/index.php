c513dfaf850372d2268fcc7adc555334c727ae11
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-03-19 01:18:52 -0400

    contact page styling
    Signed-off-by: Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>

--- contact/index.php
+++ contact/index.php
@@ -10,16 +10,18 @@ include('../header.php');
 </div>
 <div class="contact">
 	<div class="contact-container">
-		<p class="contact-page-text">Are you interested in our services? Looking for a quote to just looking into us a little bit more? Feel free to shoot us an email at <b title="Copy to Clipboard" onclick="copyemail()" style="cursor: pointer;">sales@primestudios.co</b> and we will respond to you within one business day!
-		</p>
-		<p class="business-hours-header"><i><b>Business Hours</b></i></p>
+		<div class="contact-top">
+			<img src="/assets/img/contact.svg" />
+		<p class="contact-page-text">Are you interested in our services? Looking for a quote to just looking into us a little bit more? Feel free to shoot us an email at <b title="Copy to Clipboard" onclick="copyemail()" style="cursor: pointer;">sales@primestudios.co</b> and we will respond to you within one business day!</p>
+		</div>
+		<p class="business-hours-header">business hours</p>
 		<table style="height: 2vh; margin-bottom: 2vh;">
 			<tr class="package-header-table">
-				<th><b>Mon</b></th>
-				<th><b>Tue</b></th>
-				<th><b>Wed</b></th>
-				<th><b>Thu</b></th>
-				<th><b>Fri</b></th>
+				<th>Monday</th>
+				<th>Tuesday</th>
+				<th>Wednesday</th>
+				<th>Thursday</th>
+				<th>Friday</th>
 			</tr>
 			<tr class="package-table-white">
 				<th>9AM-5PM EST</th>
@@ -31,24 +33,15 @@ include('../header.php');
 		</table>
 		<div id="contact_form" class="contact-form">
 			<form id="contactform" enctype="text/plain" action="../includes/contact.php" method="get">
-				<span class="contact-title">Name:</span><br>
+				<span class="contact-title"><i class="fas fa-user-tag"></i>name</span>
 				<input type="text" name="name"><br>
-				<span class="contact-title">E-mail:</span><br>
+				<span class="contact-title"><i class="fas fa-envelope"></i>email address</span>
 				<input type="text" name="email"><br>
-				<span class="contact-title">Requirements:</span><br><br>
-				<input id="webdesign" type="checkbox" name="webdesign" size="50">
-				<label for="webdesign">Web Design</label><br>
-				<input id="sysadmin" type="checkbox" name="sysadmin" size="50">
-				<label for="sysadmin">Sys Admin</label><br>
-				<input id="seo" type="checkbox" name="seo" size="50">
-				<label for="seo">SEO</label><br>
-				<input id="other" type="checkbox" name="other" size="50">
-				<label for="other">Other (Put in comments)</label><br><br>
-				<span class="contact-title">Budget:</span><br>
+				<span class="contact-title"><i class="fas fa-money-bill-wave"></i>budget</span>
 				<input type="number" name="budget" size="50" min="0"><br>
-				<span class="contact-title">Comments:</span><br>
-				<input type="text" name="comments" size="50"><br><br>
-				<input type="submit" value="Submit">
+				<span class="contact-title"><i class="fas fa-comments"></i>comments</span>
+				<input type="text" name="comments" size="50"><br>
+				<input type="submit" value="submit" class="contact-submit">
 			</form>
 		</div>
 	</div>
