2ecc05932255e08ede96bea759cad412c811ad79
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-04-18 21:38:47 -0400

    Spelling fix

--- success/index.php
+++ success/index.php
@@ -9,11 +9,11 @@ include('../header.php'); ?>
 	<div class="success">
 		<h2><i class="fas fa-check-circle" style="color: green; font-size: 1rem"></i> You successfully sent your message to us!</h2>
 		<h3>What does this mean?</h3>
-		<p>Your inquery has successfully reached our inbox!</p>
+		<p>Your inquiry has successfully reached our inbox!</p>
 		<h3>What now?</h3>
 		<p>You should keep an eye on your inbox awaiting a reply from us within one business day of the message being sent.</p>
 		<h3>Made an error?</h3>
-		<p>Feel free to go ahead and resend your inquery and mention that you made in an error in the one you sent prior and we will review the new one instead and address that one only.</p>
+		<p>Feel free to go ahead and resend your inquiry and mention that you made in an error in the one you sent prior and we will review the new one instead and address that one only.</p>
 		<h2>We are eager to work with you and we appreciate your interest in us!</h2>
 	</div>
 	<?php include('../footer.php'); ?>
