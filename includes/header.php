080965482db9307ecefc6850f3ebfd7c2567ccef
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-21 15:09:52 -0400

    New site start

---
+++ includes/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Meta Tags -->
+		<title><?php echo $pageName;?> - Prime Studios</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+		<meta charset="UTF-8">
+		<meta name="description" content="Prime Studios is a freelance company focused on building powerful websites for our clients. We strive to provide user-friendly and affordable websites through our service. Whatever your needs, we can fill all your needs with our design team's creativity">
+		<meta name="keywords" content="Prime Studios,Web Development,Web Design,Freelancer,Web Freelancer,Web Developer">
+		<meta name="robots" content="index, follow">
+		<meta name="copyright" content="Prime Studios">
+		<meta name="language" content="EN">
+		<meta name="author" content="Prime Studios">
+		<meta name="creationdate" content="12/01/2019">
+		<meta name="distribution" content="global">
+		<meta name="rating" content="general">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge;" />
+		<!-- Favicons -->
+		<link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+		<link rel="manifest" href="/assets/img/site.webmanifest">
+		<link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#8899a6">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+		<!-- TypeIt -->
+		<script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
+		<!-- Custom Stylesheet -->
+		<link href="/assets/css/styles.css" rel="stylesheet" />
+		<link href="/assets/css/animate.css" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900" rel="stylesheet" type="text/css" />
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+		<!-- Main JavaScript -->
+		<script src="/assets/js/main.js"></script>
+		<script src="/assets/js/particles.js"></script>
+	</head>
+	<body>
+		<div class="header">
+			<img src="/assets/img/logo.png" />
+			<h1>Prime Studios</h1>
+			<ul>
+				<li><a href="/">home</a></li>
+				<li><a href="/">about</a></li>
+				<li class="dropdown-trigger">services <i class="fas fa-chevron-down"></i></li>
+				<div class="dropdown">
+					<ul>
+						<li><a href="/">Web Design</a></li>
+						<li><a href="/">Graphic Design</a></li>
+						<li><a href="/">Open Source</a></li>
+					</ul>
+				</div>
+			</ul>
+			<h2><a href="/">CONTACT</a></h2>
+		</div>
--- index.php
+++ index.php
@@ -1,56 +1,185 @@
 <?php $pageName = 'Home';
-$pageLevel = 0;
-include('includes/recursivity.php');
-include('header.php'); ?>
-	<div id="home" class="hero">
-		<h1>prime studios</h1>
-		<p>Quality, affordable development and design services.</p>
-		<a onclick="Down_Arrow()"><i class="fas fa-chevron-down"></i></a>
+require_once('includes/header.php'); ?>
+<div class="home-container">
+	<div class="home-hero">
+		<div id="particles-js"></div>
+		<div class="typeit-container">
+			<h1>We Create</h1>
+			<h1><i><span id="typewriter"></span></i></h1>
+			<script>
+				new TypeIt("#typewriter", {
+				    speed: 100,
+				    deleteSpeed: 100,
+				    loop: true,
+				    waitUntilVisible: true
+				})
+				    .type("WEBSITES")
+				    .pause(1000)
+				    .delete(8)
+				    .type("APPLICATIONS")
+				    .pause(1000)
+				    .delete(12)
+				    .type("PRODUCTS")
+				    .pause(1000)
+				    .go();
+			</script>
+			<h1>That Work</h1>
+			<p onclick="Down_Arrow()">Learn More <i class="fas fa-chevron-down"></i></p>
+		</div>
+		<img src="/assets/img/adaptative.png" />
 	</div>
-	<div id="features" class="features">
-		<h2>Prime does it different</h2>
-		<div class="container">
-			<div class="card">
-				<i class="fas fa-shipping-fast"></i>
-				<h3>Fast</h3>
-				<p>No one likes waiting.
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+ 	 	<path fill="#ff9900" fill-opacity="1" d="M0,192L48,176C96,160,192,128,288,128C384,128,480,160,576,149.3C672,139,768,85,864,96C960,107,1056,181,1152,181.3C1248,181,1344,107,1392,69.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+	</svg>
+	<div id="home_about" class="home-about">
+		<div class="home-about-content">
+			<h1>Prime does it different</h1>
+			<div class="keypoints-container">
+				<div class="keypoint-container kp-padding">
+					<i class="fas fa-shipping-fast"></i>
+					<h3>Fast</h3>
+					<p>No one likes waiting.
 					<br /> Our team works diligently to bring you a quality product in a timely matter.</p>
-			</div>
-			<div class="card">
-				<i class="fas fa-user-tie"></i>
-				<h3>Professional</h3>
-				<p>We are committed to maintaining a professional relationship and producing adept work for our clients.</p>
-			</div>
-			<div class="card">
-				<i class="fas fa-award"></i>
-				<h3>Experienced</h3>
-				<p>Our team's diverse set of skills allows us to work effectively and collaboratively to ensure a quality end product.</p>
+				</div>
+				<div class="keypoint-container kp-padding">
+					<i class="fas fa-user-tie"></i>
+					<h3>Professional</h3>
+					<p>We are committed to maintaining a professional relationship and producing adept work for our clients.</p>
+				</div>
+				<div class="keypoint-container kp-padding-two">
+					<i class="fas fa-award"></i>
+					<h3>Experienced</h3>
+					<p>Our team's diverse set of skills allows us to work effectively and collaboratively to ensure a quality end product.</p>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="showcase">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-			<path fill="#FFFFFF" fill-opacity="1" d="M0,64L40,85.3C80,107,160,149,240,144C320,139,400,85,480,90.7C560,96,640,160,720,197.3C800,235,880,245,960,229.3C1040,213,1120,171,1200,138.7C1280,107,1360,85,1400,74.7L1440,64L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z">
-			</path>
-		</svg>
-		<h2>Showcase</h2>
-		<div class="container">
-			<img onclick="Showcase_Home_One()" class="showcase-item" src="assets/img/showcase-1.png">
-			<img onclick="Showcase_Home_Two()" class="showcase-item" src="assets/img/showcase-2.png">
-			<img onclick="Showcase_Home_Three()" class="showcase-item" src="assets/img/showcase-3.png">
+	<div class="home-showcase">
+		<h1>Showcase</h1>
+		<div class="showcase-container">
+			<img src="/assets/img/showcase-11.png" />
+			<img src="/assets/img/showcase-11.png" />
+			<img src="/assets/img/showcase-11.png" />	
 		</div>
-		<p class="home-project-list"><a class="home-project-link" href="showcase">view more</a></p>
+		<p><a href="/">view more</a></p>
 	</div>
-	<span onclick="Showcase_Close()" id="showcase_popup_background" class="showcase-popup-background"></span>
-	<div id="showcase_popup" class="showcase-popup animated zoomIn">
-		<p onclick="Showcase_Close()" class="showcase-popup-close">x</p>
-		<img id="showcase_popup_image_one" class="showcase-popup-image" src="assets/img/showcase-1.png">
-		<img id="showcase_popup_image_two" class="showcase-popup-image" src="assets/img/showcase-2.png">
-		<img id="showcase_popup_image_three" class="showcase-popup-image" src="assets/img/showcase-3.png">
+	<div class="home-contact">
+		<h1>Interested in our services?</h1>
+		<p><a href="/">Request a Quote</a></p>
 	</div>
-	<div class="quote">
-		<a href="contact">
-			<button>Request a Quote</button>
-		</a>
-	</div>
-	<?php include('footer.php'); ?>
+</div>
+<script>
+particlesJS('particles-js',
+ {
+  "particles": {
+    "number": {
+      "value": 6,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#1b1e34"
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 160,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 10,
+        "size_min": 40,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 200,
+      "color": "#ffffff",
+      "opacity": 1,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 8,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+);
+</script>
+<?php require_once('includes/footer.php'); ?>
