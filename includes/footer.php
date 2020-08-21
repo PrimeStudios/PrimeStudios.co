5b7d481f9d46058200e5233d922d93e0eb7de1bb
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-21 17:20:39 -0400

    main finish

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,22 @@
 	</body>
 	<footer>
+		<ul class="left">
+			<li class="subheader">Services</li>
+			<li><a href="/">Web Design</a></li>
+			<li><a href="/">Graphic Design</a></li>
+			<li><a href="/">System Administration</a></li>
+		</ul>
+		<ul class="right">
+			<li class="subheader">Company</a></li>
+			<li><a href="/">Terms of Service</a></li>
+			<li><a href="/">Privacy Policy</a></li>
+			<li><a href="/">Our Team</a></li>
+		</ul>
+		<ul class="footer-socials">
+			<li><a href="/"><i class="fab fa-github"></i></a></li>
+			<li><a href="/"><i class="fab fa-linkedin"></i></a></li>
+			<li><a href="/"><i class="fab fa-discord"></i></a></li>
+		</ul>
+		<h1>&copy; Prime Studios 2019-<script>document.write(new Date().getFullYear())</script>. All Rights Reserved.</h1>
 	</footer>
 </html>
