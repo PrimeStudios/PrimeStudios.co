d31baffad411c58152317f00c501786318ab6cad
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-23 03:44:47 -0400

    fix

--- about/index.php
+++ about/index.php
@@ -2,12 +2,12 @@
 require_once('../includes/header.php'); ?>
 <div class="about-container">
 	<div class="about-navbar">
-		<a href="/"><p class="about-active">Introduction</p></a>
+		<a href="/about"><p class="about-active">Introduction</p></a>
 		<a href="/"><p>Our Team</p></a>
 		<a href="/"><p>Our History</p></a>
 	</div>
 	<div class="about-content">
-		<h1>Who we are</h1>
+		<h1>Who We Are</h1>
 		<p>At Prime Studios, we focus on putting the client first. We strive to deliver a professional and adept product to our clients. Our team has many years of experience and skills that allow us to preform to the industry's standard - and excel in it.</p>
 		<h1>Our Strategy</h1>
 		<p>Throughout our operations, we are committed to maintaining a professional relationship with our clients. We understand that those who work with us put the future of their projects in our hands - and we intend to safeguard it.</p>
