6be2268ae62584c01d4f80135b4c6bf771c244fe
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-23 04:33:54 -0400

    fix

---
+++ about/history/index.php
@@ -0,0 +1,14 @@
+<?php $pageName = 'History';
+require_once('../../includes/header.php'); ?>
+<div class="about-container">
+	<div class="about-navbar">
+		<a href="/about"><p>Introduction</p></a>
+		<a href="/about/team"><p>Our Team</p></a>
+		<a href="/about/history"><p class="about-active">Our History</p></a>
+	</div>
+	<div class="about-content-two">
+		<h1>Get to Know Our History</h1>
+		<h2>Milestones On Our Path To Where We Are Today</h2>
+	</div>
+</div>
+<?php require_once('../../includes/footer.php'); ?>
