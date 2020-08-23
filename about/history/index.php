4cb37864831c32893706da2835487e9deb2410a1
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-23 05:08:37 -0400

    fix

--- about/history/index.php
+++ about/history/index.php
@@ -9,6 +9,27 @@ require_once('../../includes/header.php'); ?>
 	<div class="about-content-two">
 		<h1>Get to Know Our History</h1>
 		<h2>Milestones On Our Path To Where We Are Today</h2>
+		<div class="year-container history-year-one">
+			<div class="history-bg" style="background-color: #ff9900;">
+				<h1>2019</h1>
+			</div>
+			<h3>Our Inception</h3>
+			<p>Prime Studios officially launched in December of 2019. It is here that our journey started with just one member of our team with a dream to succeed. We got a foundation and basic site down and arranged our packages and services that we would initially offer as we got the company off of it's feet.</p>
+		</div>
+		<div class="year-container history-year-two">
+			<div class="history-bg" style="background-color: #ff9900;">
+				<h1>2020</h1>
+			</div>
+			<h3>Our Inception</h3>
+			<p>Officially opening our doors. Still writing the books of history. We start the year off strong with one goal in mind to obtain as many clients as possible to start building our brand. We also brought a second person onto the team in form of a CIO in charge of infrastructure related operations. With this also came a new refreshing design for our site.</p>
+		</div>
+		<div class="year-container history-year-three">
+			<div class="history-bg" style="background-color: #ff9900;">
+				<h1>20++</h1>
+			</div>
+			<h3>Our Inception</h3>
+			<p>Here we look to the future and are looking forward to expanding fast. We have future focuses on more then just web development and marketing advice. We aim to open a data center and expand into many other industries</p>
+		</div>
 	</div>
 </div>
 <?php require_once('../../includes/footer.php'); ?>
