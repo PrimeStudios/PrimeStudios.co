9ab12da31b124ccfe6113ce8ad127c2288a86660
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-23 03:34:31 -0400

    FIX

--- about/index.php
+++ about/index.php
@@ -6,5 +6,11 @@ require_once('../includes/header.php'); ?>
 		<a href="/"><p>Our Team</p></a>
 		<a href="/"><p>Our History</p></a>
 	</div>
+	<div class="about-content">
+		<h1>Who we are</h1>
+		<p>At Prime Studios, we focus on putting the client first. We strive to deliver a professional and adept product to our clients. Our team has many years of experience and skills that allow us to preform to the industry's standard - and excel in it.</p>
+		<h1>Our Strategy</h1>
+		<p>Throughout our operations, we are committed to maintaining a professional relationship with our clients. We understand that those who work with us put the future of their projects in our hands - and we intend to safeguard it.</p>
+	</div>
 </div>
 <?php require_once('../includes/footer.php'); ?>
