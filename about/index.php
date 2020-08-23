0c1202276a331635e26c316b0034930576e4bd1a
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-23 01:00:55 -0400

    fix

--- about/index.php
+++ about/index.php
@@ -1,5 +1,10 @@
 <?php $pageName = 'About';
 require_once('../includes/header.php'); ?>
 <div class="about-container">
+	<div class="about-navbar">
+		<a href="/"><p class="about-active">Introduction</p></a>
+		<a href="/"><p>Our Team</p></a>
+		<a href="/"><p>Our History</p></a>
+	</div>
 </div>
 <?php require_once('../includes/footer.php'); ?>
