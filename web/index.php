6f389d7f6b6f0aa90f3bf0c7d0bdbcb3432c5619
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-25 18:13:11 -0400

    fix

--- web/index.php
+++ web/index.php
@@ -14,16 +14,16 @@ require_once('../includes/header.php'); ?>
 	<p>We don't have set plans or pricing, our team works tailored to your needs. No matter how simple or complex your next project, we got you covered.</p>
 	<h2>What we work with</h2>
 	<div class="technologies">
-		<img src="/assets/img/technologies/bootstrap.png" />
-		<img src="/assets/img/technologies/css.png" />
-		<img src="/assets/img/technologies/foundation.png" />
-		<img src="/assets/img/technologies/html.png" />
-		<img src="/assets/img/technologies/javascript.png" />
-		<img src="/assets/img/technologies/jquery.png" />
-		<img src="/assets/img/technologies/mysql.png" />
-		<img src="/assets/img/technologies/nodejs.png" />
-		<img src="/assets/img/technologies/php.png" />
-		<img src="/assets/img/technologies/uikit.png" />
+		<img title="Bootstrap" src="/assets/img/technologies/bootstrap.png" />
+		<img title="CSS" src="/assets/img/technologies/css.png" />
+		<img title="Foundation" src="/assets/img/technologies/foundation.png" />
+		<img title="HTML" src="/assets/img/technologies/html.png" />
+		<img title="Javascript" src="/assets/img/technologies/javascript.png" />
+		<img title="JQuery" src="/assets/img/technologies/jquery.png" />
+		<img title="MySQL" src="/assets/img/technologies/mysql.png" />
+		<img title="NodeJS" src="/assets/img/technologies/nodejs.png" />
+		<img title="PHP" src="/assets/img/technologies/php.png" />
+		<img title="UIKit" src="/assets/img/technologies/uikit.png" />
 	</div>
 </div>
 <?php require_once('../includes/footer.php'); ?>
