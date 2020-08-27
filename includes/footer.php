8f21364a3b609a65f20652d54dde7dd3fb06fcca
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-08-26 23:59:50 -0400

    Fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 	</body>
 	<footer>
 		<p onclick="scrollTopFunction()"><i class="fas fa-arrow-circle-up"></i> back to the top</p>
+		<h2>Navigation</h2>
 		<ul class="left">
 			<li class="subheader">Services</li>
 			<li><a href="/web">Web Design</a></li>
@@ -9,16 +10,17 @@
 		</ul>
 		<ul class="right">
 			<li class="subheader">Company</a></li>
-			<li><a href="/tos">Terms of Service</a></li>
-			<li><a href="/privacy">Privacy Policy</a></li>
 			<li><a href="/about/team">Our Team</a></li>
+			<li><a href="/opensource">Open Source</a></li>
+			<li><a href="/showcase">Showcase</a></li>
 		</ul>
+		<hr />
 		<ul class="footer-socials">
 			<li><a href="https://github.com/PrimeStudios" target="_blank"><i class="fab fa-github"></i></a></li>
 			<li><a href="https://www.linkedin.com/company/prime-studios-official/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
 			<li><a href="https://discord.gg/z7wScKW" target="_blank"><i class="fab fa-discord"></i></a></li>
 		</ul>
-		<h1>&copy; Prime Studios 2019-<script>document.write(new Date().getFullYear())</script>. All Rights Reserved.</h1>
+		<h1>&copy; <b>Prime Studios</b> 2019-<script>document.write(new Date().getFullYear())</script>. All Rights Reserved. | <a href="/tos">TERMS</a> | <a href="/privacy">POLICY</a> | <a href="/contact">CONTACT</a> |</h1>
 	</footer>
 	<div id="cookie_notice" class="cookies-container animated slideInUp">
 		<p>We use cookies to personalise content and ads, to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media.<br /><a class="cookie-link" href="/privacy">Learn more</a></p>
