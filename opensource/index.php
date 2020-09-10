5ead46cea9d652c6a17cbf7a05c0f194e1773d01
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-09-10 05:34:50 -0400

    fix

--- opensource/index.php
+++ opensource/index.php
@@ -9,6 +9,34 @@ require_once('../includes/header.php'); ?>
 	<div class="breadcromb">
 		<p><a href="/"><i class="fas fa-home"></i> Home</a> / <b>Open Source</b></p>
 	</div>
-	<h1>Coming Soon</h1>
+	<div class="opensource-container">
+		<div class="opensource-navbar">
+			<p>SteamGroup Count JS</p>
+			<div class="project-links-container">
+				<h2><a href="https://github.com/PrimeStudios/Steam-Group-Members-JS" target="_blank"><i class="fab fa-github"></i></a></h2>
+				<h2><a href="https://github.com/PrimeStudios/Steam-Group-Members-JS/archive/master.zip" target="_blank"><i class="fas fa-download"></i></a></h2>
+			</div>
+			<p>Discord Webhooks</p>
+			<div class="project-links-container">
+				<h2><a href="https://github.com/PrimeStudios/Discord-Webhooks" target="_blank"><i class="fab fa-github"></i></a></h2>
+				<h2><a href="https://github.com/PrimeStudios/Discord-Webhooks/archive/master.zip" target="_blank"><i class="fas fa-download"></i></a></h2>
+			</div>
+			<p>Contact API</p>
+			<div class="project-links-container">
+				<h2><a href="https://github.com/PrimeStudios/Contact-API" target="_blank"><i class="fab fa-github"></i></a></h2>
+				<h2><a href="https://github.com/PrimeStudios/Contact-API/archive/master.zip" target="_blank"><i class="fas fa-download"></i></a></h2>
+			</div>
+			<p>Website</p>
+			<div class="project-links-container">
+				<h2><a href="https://github.com/PrimeStudios/PrimeStudios.co" target="_blank"><i class="fab fa-github"></i></a></h2>
+				<h2><a href="https://github.com/PrimeStudios/PrimeStudios.co/archive/master.zip" target="_blank"><i class="fas fa-download"></i></a></h2>
+			</div>
+		</div>
+		<div class="opensource-content">
+			<h1>Open Source</h1>
+			<img src="/assets/img/opensource.png" />
+			<p>Here you can find our open-source projects to use at your discretion. Use the navbar to the left to view projects and get a quick read on what they are about as well as related links. We ask that credit be given for using portions/entire projects if they are republished.</p>
+		</div>
+	</div>
 </div>
 <?php require_once('../includes/footer.php'); ?>
