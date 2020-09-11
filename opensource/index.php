35382e4ae42f7e031c6fe223cbe54e594ec702b4
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-09-11 16:32:06 -0400

    fix

--- opensource/index.php
+++ opensource/index.php
@@ -35,13 +35,16 @@ require_once('../includes/header.php'); ?>
 		<div class="opensource-content">
 			<div id="os-landing">
 				<h1>Open Source</h1>
-				<img src="/assets/img/opensource.png" />
+				<img class="os-img" src="/assets/img/opensource.png" />
 				<p>Here you can find our open-source projects to use at your discretion. Use the navbar to the left to view projects and get a quick read on what they are about as well as related links. We ask that credit be given for using portions/entire projects if they are republished.</p>
 			</div>
 			<div id="os-sgc" class="animated fadeIn hidden">
 				<h1>Steam Group Count JS</h1>
 				<h2>1 / 28 / 2020</h2>
 				<p>A simple JavaScript example for displaying a Steam Group's member count on your site using the Fetch API and Abdolence's X2JS Library.</p>
+				<a href="https://www.codefactor.io/repository/github/primestudios/steam-group-members-js" target="_blank">
+					<img src="https://camo.githubusercontent.com/e93dd77c12879b7a52f5aa720e7b894dd268ef0a/68747470733a2f2f7777772e636f6465666163746f722e696f2f7265706f7369746f72792f6769746875622f7072696d6573747564696f732f737465616d2d67726f75702d6d656d626572732d6a732f6261646765" />
+				</a>
 			</div>
 			<div id="os-discord" class="animated fadeIn hidden">
 				<h1>Discord Webhooks</h1>
@@ -52,11 +55,20 @@ require_once('../includes/header.php'); ?>
 				<h1>Contact API</h1>
 				<h2>2 / 12 / 2020</h2>
 				<p>A Node.js application that handles contact inquires made on the Prime Studios Contact Page using Express.js and request-promise.</p>
+				<a href="https://www.codefactor.io/repository/github/primestudios/contact-api" target="_blank">
+					<img src="https://camo.githubusercontent.com/f18dce82dc41957f3a1c135cb8ed3c0c6a470627/68747470733a2f2f7777772e636f6465666163746f722e696f2f7265706f7369746f72792f6769746875622f7072696d6573747564696f732f636f6e746163742d6170692f6261646765" />
+				</a>
 			</div>
 			<div id="os-website" class="animated fadeIn hidden">
 				<h1>Website</h1>
 				<h2>1 / 17 / 2020</h2>
-				<p>This repository contains the project files for Prime Studios' Website.</p>
+				<p>This repository contains the project files for Prime Studios' Website. We've made the very site you see before you public to use as referrence and also as a testimonial to our work. Enjoy seeing first hand what the product we bring to you looks like and see how we do things.</p>
+				<a href="https://app.codacy.com/gh/PrimeStudios/PrimeStudios.co/dashboard" target="_blank">
+					<img src="https://camo.githubusercontent.com/409a3ac2b1c877b76353c0fd00729afb81806741/68747470733a2f2f6170692e636f646163792e636f6d2f70726f6a6563742f62616467652f47726164652f3063653936363930363938303435313639373131623562656639376530326639" />
+				</a>
+				<a href="https://www.codefactor.io/repository/github/primestudios/primestudios.co" target="_blank">
+					<img src="https://camo.githubusercontent.com/9c31c97a1a430e4dcbded57acecb7dc44e5a8140/68747470733a2f2f7777772e636f6465666163746f722e696f2f7265706f7369746f72792f6769746875622f7072696d6573747564696f732f7072696d6573747564696f732e636f2f6261646765" />
+				</a>
 			</div>
 		</div>
 	</div>
